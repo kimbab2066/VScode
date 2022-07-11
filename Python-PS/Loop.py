e59e317be5a2eeb4a36d6d8eb64a2c9aea41b635
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-07-11 16:17:39 +0900

    Loop 파이썬 ver.

--- Python-PS/Loop.py
+++ Python-PS/Loop.py
@@ -62,14 +62,65 @@ for i in range(1,T+1):
     print("Case #%d: %d + %d = %d"%(i,A,B,A+B))
 '''
 #2438   별찍기-1
-''''''
+'''
+import sys
+N = int(sys.stdin.readline())
+for i in range(1,N+1):
+    print("*"*i)
+
+#for i in range(1,int(input())+1): print("*"*i)
+#https://www.acmicpc.net/source/17259734
+'''
 #2439   별찍기-2
-''''''
+'''
+N = int(input())
+for i in range(1,N+1):
+    print(" "*(N-i) + "*"*i)
+#https://www.acmicpc.net/source/16540940
+'''
 #10871  X보다 작은 수
-''''''
+'''
+
+from posixpath import split
+
+
+N, X = map(int,input().split())
+list = map(int, input().split())
+for i in list:
+    if(X>i):
+        print(i,end=" ")
+
+score = [x for x in input().split() if int(x) < X] 
+print(' '.join(score))
+
+smaller_than_x = []
+for i in list:
+    if i < X:
+        smaller_than_x.append(i)
+print(' '.join(map(str, smaller_than_x)))
+'''
 #10952  A+B-5
-''''''
+'''
+while(True):
+    A,B = map(int,input().split())
+    if(A == 0 and B == 0): break
+    print(A+B)
+'''
 #10951  A+B-4
-''''''
+'''
+import sys 
+for i in sys.stdin:
+    A,B = map(int,i.split())
+    print(A+B)
+'''
 #1110   더하기 사이클
-''''''
+'''
+N = int(input())
+cnt = 0
+copy = N    
+while(True):
+    N = (N % 10 * 10) + (N // 10 + N % 10) % 10
+    cnt += 1 
+    if N == copy: break
+print(cnt)
+'''
